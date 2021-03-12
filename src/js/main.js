74332759166a73b0f4917c07c0903babb1534189
Artem <31157501+artemrusaleev@users.noreply.github.com>
2021-03-12 11:25:16 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const maxSlide = slides.length
   const burgerButton = document.querySelector('#burger__icon')
   const headerMenu = document.querySelector('.header__nav')
+  const toTop = document.getElementById('toTop');
   let currentSlide = 0
   arrow.forEach((el) => {
     el.addEventListener("click", (e) => {
@@ -40,4 +41,21 @@ document.addEventListener("DOMContentLoaded", () => {
     headerMenu.classList.toggle('burger__open')
     document.querySelector('.header').classList.toggle('before')
   })
+  toTop.addEventListener('click',(e)=>{
+    event.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  })
 })
+document.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    let parentScroll = document.body.scrollHeight - document.body.clientHeight;
+    let toTop = document.getElementById('toTop')
+    if (Math.round((scroll / parentScroll) * 100) > 30) {
+        toTop.classList.add('show')
+    } else {
+        toTop.classList.remove('show')
+    }
+});
